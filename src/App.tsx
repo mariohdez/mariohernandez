3f2b26c2db4c6af9958eea2b3edd9cc86a0b062b
Mario Hernandez <mhmario963@gmail.com>
2025-01-09 19:14:46 -0500

    fix App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,10 @@ function App() {
 
   return (
     <>
-    <head>
-      <title>Mario Hernandez</title>
-    </head>
-    <body>
       <h1>Mario Hernandez</h1>
       <p>
         Software engineer, based in New York city.
       </p>
-    </body>
     </>
   )
 }
